b56857af161c167d1746402a1e0aeb4e6174a696
abderrahm4ne <abderrahmaneita@gmail.com>
2025-09-13 22:39:55 +0100

    fb

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -7,41 +9,36 @@ export default function NavBar() {
   return (
     <div className="flex flex-col min-h-screen ">
 
-      <div className="w-full bg-primary flex flex-row items-center py-4 px-6 justify-between border-b">
+      <div className="w-full bg-primary flex flex-row items-center py-3 px-20 justify-between border-b">
 
         {/* Left - Logo */}
-        <div className="flex-1 flex justify-start">
-          <div className="text-5xl font-bold font-logo c-red">/////</div>
+        <div className="flex-1 flex justify-start tracking-wider">
+          <div className="text-5xl font-bold font-logo brand-red">// // //</div>
         </div>
 
         {/* Center - Navigation Links */}
         <div className="flex-1 flex justify-center">
           <div className="hidden md:flex space-x-3.5">
 
-            <NavLink to="#" className="font-bold font-routes text-routes text-sm">HOME</NavLink>
-            <NavLink to="#" className="font-bold font-routes text-routes text-sm">PRODUCTS</NavLink>
-            <NavLink to="#" className="font-bold font-routes text-routes text-sm">ABOUT US</NavLink>
-            <NavLink to="#" className="font-bold font-routes text-routes text-sm">CONTACT</NavLink>
+            <NavLink to="#" className="font-bold routes font-routes text-black text-[1.1rem]">HOME</NavLink>
+            <NavLink to="#" className="font-bold routes font-routes text-black text-[1.1rem]">PRODUCTS</NavLink>
+            <NavLink to="#" className="font-bold routes font-routes text-black text-[1.1rem]">ABOUT US</NavLink>
+            <NavLink to="#" className="font-bold routes font-routes text-black text-[1.1rem]">CONTACT</NavLink>
 
             
           </div>
         </div>
 
         {/* Right - Icons */}
-        <div className="flex-1 flex justify-end items-center space-x-6">
-        
+        <div className="flex-1 flex justify-end items-center space-x-3">
+          
+          <ShoppingCartIcon style={{ fontSize: '1.9rem' }}  className="text-black cursor-pointer routes" />
+          <SearchIcon style={{ fontSize: '1.9rem' }} className="text-black cursor-pointer routes" />
+
         </div>
       </div>
 
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <div className="md:hidden bg-gray-800 px-6 py-4 space-y-4 border-b border-gray-700">
-          <NavLink to="#" className="font-bold font-routes text-routes text-sm">HOME</NavLink>
-          <NavLink to="#" className="font-bold font-routes text-routes text-sm">PRODUCTS</NavLink>
-          <NavLink to="#" className="font-bold font-routes text-routes text-sm">ABOUT US</NavLink>
-          <NavLink to="#" className="font-bold font-routes text-routes text-sm">CONTACT</NavLink>
-        </div>
-      )}
+      
 
       </div>
   );
