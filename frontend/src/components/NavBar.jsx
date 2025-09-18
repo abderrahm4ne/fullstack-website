a0e6a90ed5723bfbffad74615bd9aa65c728b5d1
abderrahm4ne <abderrahmaneita@gmail.com>
2025-09-18 14:07:38 +0100

    adv

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,21 +11,26 @@ export default function NavBar() {
   return (
     <div className="flex flex-col ">
 
-      <div className="w-full bg-gradient-to-r from-[#e9e9e5] to-[#868686] flex flex-row items-center py-3 xl:px-20 md:px-7 px-5  justify-between border-b">
+      <div className="w-full bg-gradient-to-r from-[#1a1a1a] via-[#2c0101] to-black flex flex-row items-center py-3 xl:px-20 md:px-7 px-5  justify-between border-b border-[#f8f3e9]">
 
         {/* Left - Logo */}
         <div className="flex-1 flex justify-start tracking-wider">
-          <div className="xl:text-6xl text-5xl font-logo brand-red">Nadzheee</div>
+
+          <div className="xl:text-6xl text-5xl font-logo creamy"
+          style={{ textShadow: '-3px 3px 6px rgba(0, 0, 0, 0.8)' }}>
+            Nadzheee
+          </div>
+
         </div>
 
         {/* Center - Navigation Links */}
         <div className="flex-1 flex justify-center">
           <div className="hidden sm:flex md:space-x-3.5 space-x-2">
 
-            <NavLink to="" className="font-bold routes font-routes brand-red xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">HOME</NavLink>
-            <NavLink to="products" className="font-bold routes font-routes brand-red xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">PRODUCTS</NavLink>
-            <NavLink to="about" className="font-bold text-center routes font-routes brand-red xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">ABOUT US</NavLink>
-            <NavLink to="contact" className="font-bold routes font-routes brand-red xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">CONTACT</NavLink>
+            <NavLink to="" className="routes font-routes creamy xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">HOME</NavLink>
+            <NavLink to="products" className="routes font-routes creamy xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">PRODUCTS</NavLink>
+            <NavLink to="about" className="text-center routes font-routes creamy xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">ABOUT US</NavLink>
+            <NavLink to="contact" className="routes font-routes creamy xl:text-[1.2rem] sm:text-[1.1rem] text-[1rem]">CONTACT</NavLink>
 
           </div>
         </div>
@@ -33,14 +38,14 @@ export default function NavBar() {
         {/* Right - Icons */}
         <div className="flex-1 flex justify-end items-center space-x-3">
 
-          <ShoppingCartIcon style={{ fontSize: '2.1rem' }}  className=" brand-red cursor-pointer routes" />
-          <SearchIcon style={{ fontSize: '2.1rem' }} className=" brand-red cursor-pointer routes" />
+          <ShoppingCartIcon style={{ fontSize: '2.1rem' }}  className=" creamy cursor-pointer routes" />
+          <SearchIcon style={{ fontSize: '2.1rem' }} className=" creamy cursor-pointer routes" />
 
           <button className="sm:hidden" onClick={() => setMenuOpen(!menuOpen)}>
             {menuOpen ? (
-              <CloseIcon style={{ fontSize: '2.1rem' }} className=" brand-red cursor-pointer routes" />
+              <CloseIcon style={{ fontSize: '2.1rem' }} className=" creamy cursor-pointer routes" />
             ) : (
-              <MenuIcon style={{ fontSize: '2.1rem' }} className=" brand-red cursor-pointer routes" />
+              <MenuIcon style={{ fontSize: '2.1rem' }} className=" creamy cursor-pointer routes" />
             )}
           </button>
 
@@ -49,7 +54,7 @@ export default function NavBar() {
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="sm:hidden bg-primary border-b flex flex-col ">
+        <div className="sm:hidden bg-gradient-to-r from-[#1a1a1a] via-[#2c0101] to-black border-b flex flex-col ">
           <NavLink to="home" className="font-bold ham-menu text-center font-routes text-black  brand-red text-[1rem] py-3 border-b w-full" onClick={() => setMenuOpen(false)}>HOME</NavLink>
           <NavLink to="products" className="font-bold ham-menu text-center font-routes  brand-red text-[1rem] py-3 border-b w-full" onClick={() => setMenuOpen(false)}>PRODUCTS</NavLink>
           <NavLink to="about" className="font-bold ham-menu text-center font-routes  brand-red text-[1rem] py-3 border-b w-full" onClick={() => setMenuOpen(false)}>ABOUT US</NavLink>
@@ -59,7 +64,7 @@ export default function NavBar() {
 
       <Outlet />
 
-      <div className="w-full bg-[#1b0000] text-white text-center py-4 text-xl font-routes">
+      <div className="w-full bg-[#130101] text-white text-center py-4 text-xl font-routes">
         Nadzheee is an Algerian brand © 2025.
       </div>
 
